0986b5f8cfff4dff22895a8d5b559b8fb5e76131
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-20 11:49:39 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => setUrl(data.message));
+  }, []);
+  if (!url) return <p>Loading...</p>;
+
+  return <img alt="A Random Dog" src={url} />;
+}
+
+export default App;
